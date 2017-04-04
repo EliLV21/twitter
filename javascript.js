2db44e8f79b9119cb6e2aa5aa5545c24fa5a1e25
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-04-04 08:55:15 -0500

    fallo total

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var publicaciones = [];
+var contador = 1;
+
+function contador(){
+  var publicacion = document.getElementById("contador").value;
+  var letras = publicaciones.length;
+  var contador = 
+  //document.createTextNode()
+    //for(var i=0;i=>letras;i++){
+      //  document.getElementById("numLetras").innerHTML = letras;
+    //}
+    //document.forms[0].numLetras.value=document.forms[0].textoTweets.value.length
+  };
+
+function tweets(seccion,texto,remitente,muestra,muestra1){
+  var seccion = document.createElement("section");
+  var texto = document.getElementById("textoTweets").value;
+  var remitente = document.getElementById("contacto").value;
+  var muestra = document.createTextNode(texto);
+  var muestra1 = document.createTextNode(texto);
+
+  seccion.appendChild(muestra);
+  seccion.appendChild(muestra1);
+};
+//function(){};
